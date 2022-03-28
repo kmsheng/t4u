ca75483b133210f70964f8ffe271ad50404fb6c2
kmsheng <kmsh3ng@gmail.com>
2022-03-28 20:59:19 +0800

    add components/AppNav.vue

---
+++ src/components/AppNav.vue
@@ -0,0 +1,14 @@
+<script setup>
+import AppNav from '@/shared/AppNav.vue'
+</script>
+
+<template>
+  <app-nav>
+    <template #logo>Logo</template>
+    items
+    buttons
+  </app-nav>
+</template>
+
+<style scoped>
+</style>
