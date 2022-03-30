690b8d3ccf000c366d3d7b08544845dfc39e530a
kmsheng <kmsh3ng@gmail.com>
2022-03-31 00:31:32 +0800

    update

--- packages/ui/vite.config.js
+++ packages/ui/vite.config.js
@@ -4,5 +4,24 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'index.js'),
+      name: 'T4uUi',
+      fileName: (format) => `t4u-ui.${format}.js`
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['vue'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
 })
