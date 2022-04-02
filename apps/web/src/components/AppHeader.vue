aa8545db077c099c58ae6628ca35385ee0855ef0
kmsheng <kmsh3ng@gmail.com>
2022-04-02 23:02:05 +0800

    update

--- apps/web/src/components/AppHeader.vue
+++ apps/web/src/components/AppHeader.vue
@@ -3,16 +3,14 @@ import { TaoNav } from '@t4u/ui'
 </script>
 
 <template>
-  <tao-nav>
-    <template #logo>
-      <div>Tau</div>
-    </template>
-    <a href="">link1</a>
-    <a href="">menu</a>
-    <a href="">link1</a>
-    <a href="">link2</a>
-    <a href="">link3</a>
-  </tao-nav>
+  <div class="flex justify-between items-center px-3">
+    <a class="inline-block p-3 font-bold">Tau</a>
+    <tao-nav>
+      <a class="inline-block px-4 py-3" href="">link1</a>
+      <a class="inline-block px-4 py-3" href="">link2</a>
+      <a class="inline-block px-4 py-3" href="">link3</a>
+    </tao-nav>
+  </div>
 </template>
 
 <style scoped>
--- apps/web/src/pages/App.vue
+++ apps/web/src/pages/App.vue
@@ -1,17 +1,9 @@
 <script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import AppNav from '@/components/AppNav.vue'
-import HelloWorld from '@/components/HelloWorld.vue'
+import AppHeader from '@/components/AppHeader.vue'
 </script>
 
 <template>
-  <div class="bg-white dark:bg-slate-900">
-    <app-nav />
-    <img alt="Vue logo" src="@/assets/logo.png" />
-    <HelloWorld class="text-black dark:text-white" msg="Hello Vue 3 + Vite" />
+  <div class="bg-white dark:bg-slate-900 text-black dark:text-white">
+    <app-header />
   </div>
 </template>
-
-<style>
-</style>
