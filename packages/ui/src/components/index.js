4fef0c8f61750ce9e64f5de8839023806722a4c1
kmsheng <kmsh3ng@gmail.com>
2022-04-05 21:02:53 +0800

    update

--- packages/ui/src/components/index.js
+++ packages/ui/src/components/index.js
@@ -1,9 +1,11 @@
 import TBackdrop from './backdrop/Backdrop.vue'
 import TNav from './nav/Nav.vue'
 import TNavLink from './navLink/NavLink.vue'
+import TNavRouterLink from './navRouterLink/NavRouterLink.vue'
 
 export {
   TBackdrop,
   TNav,
-  TNavLink
+  TNavLink,
+  TNavRouterLink
 }
---
+++ packages/ui/src/components/navRouterLink/NavRouterLink.vue
@@ -0,0 +1,11 @@
+<script setup>
+import TNavLink from '@/components/navLink/NavLink.vue'
+</script>
+
+<template>
+  <router-link custom v-slot="{ navigate, href, route }">
+    <t-nav-link :href="href" @click="navigate">
+      <slot />
+    </t-nav-link>
+  </router-link>
+</template>
