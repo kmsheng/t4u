0392786cf2bd20f8bad6dae65fc84fb609fde437
kmsheng <kmsh3ng@gmail.com>
2022-04-05 18:23:58 +0800

    update

--- apps/web/src/components/AppHeader.vue
+++ apps/web/src/components/AppHeader.vue
@@ -6,14 +6,20 @@ import { TNav, TNavLink, TIconGithub } from '@t4u/ui'
   <div class="flex justify-between items-center px-3">
     <router-link class="p-3 font-bold" to="/">Tau</router-link>
     <t-nav>
-      <t-nav-link href="/components">Components</t-nav-link>
-      <t-nav-link href="/utils">Utils</t-nav-link>
+      <router-link to="/components" custom v-slot="{ navigate, href, route }">
+        <t-nav-link :href="href" @click="navigate">Components</t-nav-link>
+      </router-link>
+      <router-link to="/utils" custom v-slot="{ navigate, href, route }">
+        <t-nav-link :href="href" @click="navigate">Utils</t-nav-link>
+      </router-link>
       <t-nav-link href="https://github.com" target="_blank">
         <t-icon-github class="w-5 h-5 fill-current" />
       </t-nav-link>
       <template #menu>
         <div>
-          <a class="p-3" href="/link1">link1</a>
+          <router-link to="/components" custom v-slot="{ navigate, href, route }">
+            <a class="p-3" :href="href" @click="navigate">link1</a>
+          </router-link>
           <a class="p-3" href="/link2">link2</a>
           <a class="p-3" href="/link3">link3</a>
         </div>
