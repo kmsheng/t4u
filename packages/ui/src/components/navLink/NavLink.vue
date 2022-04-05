bf6195c254ba7fbaa054deae315431ff5c785325
kmsheng <kmsh3ng@gmail.com>
2022-04-05 13:52:28 +0800

    update

--- packages/ui/src/components/navLink/NavLink.vue
+++ packages/ui/src/components/navLink/NavLink.vue
@@ -1,6 +1,5 @@
 <template>
-  <a class="px-4 py-3 transition-color duration-300
-    hover:text-light-text-primary dark:hover:text-dark-text-primary">
+  <a class="nav-link">
     <slot />
   </a>
 </template>
