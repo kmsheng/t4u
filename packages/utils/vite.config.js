87b91641cb7468f5c4297ed45e32de1c8f50f977
kmsheng <kmsh3ng@gmail.com>
2022-04-05 22:35:32 +0800

    update

--- packages/utils/vite.config.js
+++ packages/utils/vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'index.js'),
+      entry: path.resolve(__dirname, 'src/index.js'),
       name: 'T4uUtils',
       fileName: (format) => `t4u-utils.${format}.js`
     }
