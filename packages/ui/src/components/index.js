d2c29a9e2fc4d310806c021183ab99ab20a44c61
kmsheng <kmsh3ng@gmail.com>
2022-04-05 21:22:01 +0800

    add NavMenu

--- packages/ui/src/components/index.js
+++ packages/ui/src/components/index.js
@@ -1,11 +1,13 @@
 import TBackdrop from './backdrop/Backdrop.vue'
 import TNav from './nav/Nav.vue'
+import TNavMenu from './navMenu/NavMenu.vue'
 import TNavLink from './navLink/NavLink.vue'
 import TNavRouterLink from './navRouterLink/NavRouterLink.vue'
 
 export {
   TBackdrop,
   TNav,
+  TNavMenu,
   TNavLink,
   TNavRouterLink
 }
---
+++ packages/ui/src/components/navMenu/NavMenu.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="nav-menu">
+    <div class="nav-menu__content">
+      <slot />
+    </div>
+  </div>
+</template>
